f1a62721808ded5cc3c8c273b89210efb1976855
SamNord <53309440+SamNord@users.noreply.github.com>
2019-08-24 18:19:47 +0200

    version complète

--- actviteBlog/src/app/post-list-item/post-list-item.component.ts
+++ actviteBlog/src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListItemComponent implements OnInit {
 
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts1: number = 0;
+  @Input() created_at: Date;
+  @Input() loveIts2: number = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  good() {
+
+    this.loveIts1 += 1;
+    console.log(this.loveIts1);
+  }
+
+  notGood() {
+
+    this.loveIts2 += 1;
+    console.log(this.loveIts2);
+  }
+
+
 }
+
+
+
+
+
+
+
--- actviteBlog/src/app/post-list/post-list.component.ts
+++ actviteBlog/src/app/post-list/post-list.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts = [
+    {
+      title: "Mon premier post",
+      content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+      created_at: new Date()
+    },
 
-  ngOnInit() {
-  }
+    {
+      title: "Mon deuxième post",
+      content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+      created_at: new Date()
+    },
+
+    {
+      title: "Encore un autre post",
+      content: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+      created_at: new Date()
+    }
+  ]
+
+
+
+  ngOnInit() { }
+
 
 }
+
+
+
+
